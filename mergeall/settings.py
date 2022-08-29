3b1287369858962bff92bb42e654592f332987b9
nitestparmar002 <nitestparmar002@gmail.com>
2022-08-29 09:51:45 +0530

    changes in blog project and add own middlewarwe

--- mergeall/settings.py
+++ mergeall/settings.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'blog.middleware.StackOverflowMiddleware',
 ]
 
 ROOT_URLCONF = 'mergeall.urls'
