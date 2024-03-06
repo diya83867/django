1dda421bc4cca50b5532d1caebb34471be07fad3
diya83867 <eightbit974@gmail.com>
2024-03-06 16:23:30 +0530

    Change in setting file

--- mergeall/settings.py
+++ mergeall/settings.py
@@ -122,16 +122,7 @@ CHANNEL_LAYERS = {
 
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'test',
-        'HOST': 'database-1.cxqi4eki0k95.ap-south-1.rds.amazonaws.com',
-        'PORT' : '5432',
-        'USER' : 'postgres',
-        'PASSWORD' : 'mysql1234',
-    }
-} 
+
 
 
 
