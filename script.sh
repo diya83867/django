c5f5fc0bbc2b354f0acf4052a4235530c71c403c
diya83867 <eightbit974@gmail.com>
2024-03-12 21:01:58 +0530

    Add script file

---
+++ script.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#set -e
+
+#echo "Deployment started ..."
+
+# Pull the latest version of the app
+git pull origin master
+echo "New changes copied to server !"
+
+# Activate Virtual Env
+source myenv/bin/activate
+echo "Virtual env 'mb' Activated !"
+
+echo "Installing Dependencies..."
+pip install -r requirements.txt --no-input
+
+echo "Serving Static Files..."
+python3 manage.py collectstatic --noinput
+
+echo "Running Database migration"
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+# Deactivate Virtual Env
+deactivate
+echo "Virtual env 'mb' Deactivated !"
+
+# Reloading Application So New Changes could reflect on website
+pushd miniblog
+touch wsgi.py
+popd
+
+echo "Deployment Finished!"
