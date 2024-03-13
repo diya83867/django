5b533e3b1760cd4448c3b072f8b31427d9bb562d
diya83867 <eightbit974@gmail.com>
2024-03-13 16:36:45 +0530

    new .sh file

---
+++ script/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "Deployment started ..."
+# Pull the latest version of the app
+cd /var/www/virtual 
+source myenv/bin/activate
+cd /var/www/project/django
+echo "New changes copied to server !"
+git pull origin main
+echo "Install Dependencies..."
+pip install -r requirements.txt
+echo "Serving Static Files..."
+echo yes | python3 manage.py collectstatic --noinput
+echo "Running Database migration"
+python3 manage.py makemigrations
+python3 manage.py migrate
+# Deactivate Virtual Env
+deactivate
+# Restart Server
+sudo systemctl restart apache2
+echo "Deployment Finished!"
