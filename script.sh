fb6095acfef4bd5fa93424e50cecfa7a76892bf9
diya83867 <eightbit974@gmail.com>
2024-03-13 15:31:20 +0530

    script changes

--- script.sh
+++ script.sh
@@ -1,34 +1,21 @@
 #!/bin/bash
-#set -e
-
 echo "Deployment started ..."
-
 # Pull the latest version of the app
-git pull origin main
-echo "New changes copied to server !"
-
-# Activate Virtual Env
+cd /var/www/virtual 
 source myenv/bin/activate
-echo "Virtual env 'mb' Activated !"
-
-echo "Installing Dependencies..."
-pip install -r requirements.txt --no-input
-
+cd /var/www/project/django
+echo "New changes copied to server !"
+git pull origin main
+echo "Install Dependencies..."
+pip install -r requirements.txt
 echo "Serving Static Files..."
-python3 manage.py collectstatic --noinput
-
+echo yes | python3 manage.py collectstatic --noinput
 echo "Running Database migration"
 python3 manage.py makemigrations
 python3 manage.py migrate
-
 # Deactivate Virtual Env
 deactivate
-echo "Virtual env 'mb' Deactivated !"
-
-# Reloading Application So New Changes could reflect on website
-pushd miniblog
-touch wsgi.py
-popd
-
+# Restart Server
+sudo systemctl restart apache2
 echo "Deployment Finished!"
 
